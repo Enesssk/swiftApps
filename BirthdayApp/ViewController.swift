3b1909a6aabba4a0b6807872970fc76f91f1eaef
Enesssk <140720772+Enesssk@users.noreply.github.com>
2024-02-24 00:04:05 +0300

    Initial Commit

---
+++ BirthdayApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BirthdayApp
+//
+//  Created by Enes Kala on 24.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
